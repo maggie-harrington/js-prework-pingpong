44a59ca05e12e10b17f478ed532fe9f666e08a81
Maggie Harrington <maggie.harrington@gmail.com>
2017-03-10 16:59:30 -0800

    add gulp-concat, require and create task to use it in gulpfile.js

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Gulp: npm install gulp --save-dev
 Browserify:  npm install browserify --save-dev
 
 vinyl-source-stream: npm install vinyl-source-stream --save-dev
+
+gulp-concat: npm install gulp-concat --save-dev
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 
 //more dependencies will be added here.
 
@@ -8,6 +9,12 @@ var source = require('vinyl-source-stream');
 // gulp.task('myTask', function() {
 //   console.log('hello gulp');
 // });
+
+gulp.task('concatInterface', function() {
+    return gulp.src(['./js/pingpong-interface.js', '.js/signup-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
 
 gulp.task('jsBrowserify', function() {
     return browserify( {entries: ['./js/pingpong-interface.js'] })
