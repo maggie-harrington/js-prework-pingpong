2cbed434838fafccb179304827e6861ab5a9bdd1
Maggie Harrington <maggie.harrington@gmail.com>
2017-03-10 16:16:18 -0800

    install vinyl-source-stream, add task to call browserify function and run to create app.js inside a build folder

---
+++ build/js/app.js
@@ -0,0 +1,44 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// bring calculatorModule into receiving file
+var Calculator = require('./../js/pingpong.js').calculatorModule;
+
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var simpleCalculator = new Calculator("green");
+    var output = simpleCalculator.pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
+
+},{"./../js/pingpong.js":2}],2:[function(require,module,exports){
+function Calculator(skinName) {
+  this.skin = skinName;
+}
+
+Calculator.prototype.pingPong = function(goal) {
+  var output = [];
+  for (var i = 1; i<= goal; i++) {
+    if (i % 15 === 0) {
+      output.push("ping-pong");
+    } else if (i % 3 === 0) {
+      output.push("ping");
+    } else if (i % 5 === 0) {
+      output.push("pong");
+    } else {
+      output.push(i);
+    }
+  }
+  return output;
+}
+
+// exports is provided by Node and lets us export things from one file and bring them into another. Technically, our constructor function and our pingPong method have now become part of a module. A module is a group of JavaScript functions and data that comprises some sort of functionality.
+
+//Think of exports as a giant, global JavaScript object. We are creating a new property on it called calculatorModule, and we are setting this property equal to our Calculator constructor function. And because we used a prototype to attach our pingPong() method, it gets dragged along as part of Calculator and stored in the new property of exports called calculatorModule.
+
+exports.calculatorModule = Calculator;
+
+},{}]},{},[1]);
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 //more dependencies will be added here.
 
-gulp.task('myTask', function() {
-  console.log('hello gulp');
+// sample task
+// gulp.task('myTask', function() {
+//   console.log('hello gulp');
+// });
+
+gulp.task('jsBrowserify', function() {
+    return browserify( {entries: ['./js/pingpong-interface.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
 });
